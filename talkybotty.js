4675dda346675a2993563c54620e832f111f51dc
rayraykay <raymond.d.kim@outlook.com>
2017-05-14 22:24:58 -0400

    may 14 1024

--- talkybotty.js
+++ talkybotty.js
@@ -1,4 +1,24 @@
-function windowAlert(textToDisplay) {
+/*
+ Displays the message in the text box specified by the userClass
+ class name. Note that one does not have to check for existance of
+ the body.
+ 
+ @param {string} userClass The string of the class name whose text box is to be
+ displayed.
+*/
+function displayMessageToWindow (userClass) {
+	// need a better way of getting the specified text,
+	// possibly putting the index values into a hashmap
+	// keyed by the class name ('user', 'bot' etc) beforehand,
+	// not hardcoding it here
+	var classToIndexMap = {"user": 0, "bot": 1};
+	var textInputs = document.getElementsByTagName('input');
+	var toDisplay = textInputs[classToIndexMap[userClass]].value;
+	
+	windowAlert(toDisplay);
+}
+
+function windowAlert (textToDisplay) {
     if (document.body)
         window.alert(textToDisplay);
     else
